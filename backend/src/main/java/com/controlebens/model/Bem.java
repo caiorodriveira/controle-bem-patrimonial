fe5a5036b3237dfb3455389b676d5b0c1674b31b
caiorodriveira <rodrigocaio2775@gmail.com>
2024-03-18 21:48:20 -0300

    IMPROVEMENT - criação de relacionamento de bem com Local

--- backend/src/main/java/com/controlebens/model/Bem.java
+++ backend/src/main/java/com/controlebens/model/Bem.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,7 +38,9 @@ public class Bem {
 	
 	private BigDecimal valorAtual;
 	
-	private String local; //clase Local
+	@ManyToOne
+	@JoinColumn(name = "id_local")
+	private Local local; //clase Local
 	
 	@Enumerated(EnumType.STRING)
 	private EstadosBem estadoBem; //enum
